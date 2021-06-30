f7da127b80fec0a05b8c958b7e6d465289574b7b
ajoanaverde <ajoanaverde@gmail.com>
2021-06-30 10:41:56 +0200

    auth and guest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,15 +13,21 @@
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li><a href="" class="p-3">Home</a></li>
-            <li><a href="{{ route('dashboard')}}" class="p-3">Dashboard</a></li>
+            <li><a href="{{ route('dashboard') }}" class="p-3">Dashboard</a></li>
             <li><a href="" class="p-3">Post</a></li>
         </ul>
 
         <ul class="flex items-center">
-            <li><a href="" class="p-3">Jojo</a></li>
-            <li><a href="" class="p-3">Login</a></li>
-            <li><a href="{{ route('register')}}" class="p-3">Register</a></li>
-            <li><a href="" class="p-3">Logout</a></li>
+
+            @auth
+                <li><a href="" class="p-3">Jojo</a></li>
+                <li><a href="" class="p-3">Logout</a></li>
+            @endauth
+
+            @guest
+                <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
+                <li><a href="" class="p-3">Login</a></li>
+            @endguest
         </ul>
     </nav>
 
